d0a0b6ad42a7f37ed67354a8ae717b6101713071
Kanishk Gupta <78502480+kanishkg19@users.noreply.github.com>
2021-02-28 22:17:40 +0530

    Add files via upload

---
+++ Rock,paper,scissor.py
@@ -0,0 +1,47 @@
+import random
+
+#Rock Paper Scissors By KANISHK GUPTA
+def gameWin(comp, you):
+   
+    if comp == you:
+        return None
+
+    elif comp == 'p':
+        if you=='r':
+            return False
+        elif you=='s':
+            return True
+    
+    elif comp == 'r':
+        if you=='s':
+            return False
+        elif you=='p':
+            return True
+    
+    elif comp == 's':
+        if you=='p':
+            return False
+        elif you=='r':
+            return True
+
+print("Comp Turn: Rock(r) Paper(p) Scissor(s)? ")
+randNo = random.randint(1, 3) 
+if randNo == 1:
+    comp = 'p'
+elif randNo == 2:
+    comp = 'r'
+elif randNo == 3:
+    comp = 's'
+
+you = input("Your Turn: Rock(r) Paper(p) Scissor(s)? ")
+a = gameWin(comp, you)
+
+print(f"Computer chose {comp}")
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
